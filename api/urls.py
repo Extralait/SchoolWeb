7e561ae46a4f2a64e04c4310e05dac110ecd581b
Валера <tehnolog1999@gmail.com>
2020-12-18 03:41:22 +1000

    settings

--- api/urls.py
+++ api/urls.py
@@ -5,20 +5,19 @@ from .views import EventViewSet, OrganizationViewSet, CustomAuthToken
 
 router = routers.DefaultRouter()
 router.register(r'events', EventViewSet)
-router.register(r'organizations', OrganizationViewSet)
+router.register(r'specialty', OrganizationViewSet)
+
 
 from django.urls import include, path
 
 from .views import UserProfileListCreateView, userProfileDetailView
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('token/', CustomAuthToken.as_view(), name='token'),
-    path('specialty/',OrganizationViewSet.as_view(), name='specialty'),
-    #gets all user profiles and create a new profile
-    path("all-profiles",UserProfileListCreateView.as_view(),name="all-profiles"),
-   # retrieves profile details of the currently logged in user
-    path("profile/<int:pk>",userProfileDetailView.as_view(),name="profile"),
+    path("", include(router.urls)),
+    path("token/", CustomAuthToken.as_view(), name='token'),
+    # path("specialty/<int:pk>", OrganizationViewSet.as_view(), name='specialty'),
+    # gets all user profiles and create a new profile
+    path("all-profiles", UserProfileListCreateView.as_view(), name="all-profiles"),
+    # retrieves profile details of the currently logged in user
+    path("profile/<int:pk>", userProfileDetailView.as_view(), name="profile"),
 ]
-
-
