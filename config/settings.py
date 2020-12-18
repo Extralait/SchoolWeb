fd0376d7ab9e2fb71fd4882dd1b333d5fb8eedce
Валера <tehnolog1999@gmail.com>
2020-12-18 10:44:06 +1000

    Proreq change

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '/staticfiles')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
