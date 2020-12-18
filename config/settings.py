7a37ad593bd211cef9ea3bb1cdd98ed0305731a2
Валера <tehnolog1999@gmail.com>
2020-12-18 11:02:08 +1000

    Proreq change

--- config/settings.py
+++ config/settings.py
@@ -124,10 +124,17 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '/staticfiles/')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, '../staticfiles'))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, '../staticfiles'))
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '../static'),
+# )
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
